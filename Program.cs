4c6c867f06e2a267e04133fd479e72d1fc73551d
Bella Cockrell <56876663+bella-cockrell@users.noreply.github.com>
2022-12-16 16:37:24 +0100

    Add console intro

--- Program.cs
+++ Program.cs
@@ -1,8 +1,31 @@
-﻿namespace al_gores_algos;
+﻿using System.Text;
+
+namespace al_gores_algos;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string[] listOfAlgorithms = {"Binary Search"};
+        var concatListOfAlgorithms = string.Join("\n", listOfAlgorithms);
+
+        Console.WriteLine("Which algorithm would you like to try from this list?" + "\n" + concatListOfAlgorithms);
+        var userInput = Console.ReadLine();
+
+        while (!Array.Exists(listOfAlgorithms, element => element == userInput)) 
+        {
+            Console.WriteLine("That's not in the list, silly -- pick another one!");
+            userInput = Console.ReadLine();
+        }
+
+        switch(userInput)
+        {
+            case "Binary Search":
+                Console.WriteLine("Cool, you want BINARY SEARCH!");
+                break;
+
+            default:
+                Console.WriteLine("Something's gone wrong!");
+                break;
+        }
     }
 }
