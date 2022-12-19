b7c2d05cc615d90af372627a5e8a4d2936d713b3
Bella Cockrell <56876663+bella-cockrell@users.noreply.github.com>
2022-12-19 16:01:21 +0100

    Add Binary Search console interface

--- Program.cs
+++ Program.cs
@@ -4,25 +4,78 @@ using AlGoreIthms = Algorithms.Algorithms;
 namespace al_gores_algos;
 class Program
 {
+
     static void Main(string[] args)
     {
-        string[] listOfAlgorithms = {"Binary Search"};
-        var concatListOfAlgorithms = string.Join("\n", listOfAlgorithms);
+        var rejectionMessage = ">>That's not right, silly -- pick another one!";
 
-        Console.WriteLine("Which algorithm would you like to try from this list?" + "\n" + concatListOfAlgorithms);
+        var listOfAlgorithms = new Dictionary<string, string>(){
+            {"1", "Binary Search"},
+        };
+        var sb = new StringBuilder();
+        foreach (var kvp in listOfAlgorithms)
+        {
+            sb.Append($"{kvp.Key}. {kvp.Value}" + "\n");
+        }
+        var concatListOfAlgorithms = sb.ToString();
+
+        Console.WriteLine(">>Which algorithm would you like to try from this list? Input the number." + "\n" + concatListOfAlgorithms);
+
         var userInput = Console.ReadLine();
 
-        while (!Array.Exists(listOfAlgorithms, element => element == userInput)) 
+        while ((userInput != null) && (!listOfAlgorithms.ContainsKey(userInput))) 
         {
-            Console.WriteLine("That's not in the list, silly -- pick another one!");
+            Console.WriteLine(rejectionMessage);
             userInput = Console.ReadLine();
         }
 
         switch(userInput)
         {
-            case "Binary Search":
-                Console.WriteLine("Cool, you want BINARY SEARCH!");
-                //AlGoreIthms.BinarySearch;
+            case "1":
+                Console.WriteLine("\n >>Cool, you want BINARY SEARCH!\n>>Please enter a list of numbers, separated by a space\n>>E.g. 1 2 3 4");
+
+                string? userInputtedStringOfNumbers = Console.ReadLine();
+
+                var binarySearchList = new List<int>();
+
+                if (!string.IsNullOrEmpty(userInputtedStringOfNumbers))
+                {
+                    string[]? splitUserInputtedStringOfNumbers = userInputtedStringOfNumbers.Split(" ");
+
+                    foreach(string userInputtedDigit in splitUserInputtedStringOfNumbers )
+                    {
+
+                        int number;
+                        var success = Int32.TryParse(userInputtedDigit, out number);
+
+                        if (success) {
+                            binarySearchList.Add(number);
+                        } else
+                        {
+                            Console.Write($"Cannot parse '{userInputtedDigit}'");
+                        }
+                    }
+                } 
+                else 
+                {
+                    Console.WriteLine("Can't deal with null!");
+                    return;
+                }
+                
+                binarySearchList.Sort();
+                int[] binarySearchArray = binarySearchList.ToArray();
+
+                Console.WriteLine(">>This is what your list looks like after sorting" + "\n>> " + String.Join(" ", binarySearchList));
+                Console.WriteLine(">>Which number do you want to find the position of?");
+                var userInputtedIndex = Console.ReadLine();
+
+                int indexNumber;
+                var indexSuccess = Int32.TryParse(userInputtedIndex, out indexNumber);
+                if (indexSuccess)
+                {
+                    var result = AlGoreIthms.BinarySearch(binarySearchArray, indexNumber);
+                    Console.Write($"The index is {result}");
+                }
                 break;
 
             default:
